11cb64541630a506b6806a047b1a595661a0e0be
Gordon Anderson <gordon.b.anderson@gmail.com>
2021-12-16 20:51:15 +0000

    ENHANCEMENT: Build assets using laravel mix

---
+++ webpack.mix.js
@@ -0,0 +1,46 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.combine([
+    'client/resources/js/leaflet.js',
+    'client/resources/js/leaflet-search.js',
+    'client/resources/js/leaflet.draw.1.0.4.js',
+    'client/resources/js/proj4.js',
+    'client/resources/js/wicket.js',
+    'client/resources/js/MapField.js',
+    ], 'client/dist/js/mapfield.js')
+
+    .combine([
+        'client/resources/css/leaflet.css',
+        'client/resources/css/leaflet-search.css',
+        'client/resources/css/leaflet.draw.1.0.4.css',
+        'client/resources/css/MapField.css'
+    ], 'client/dist/js/mapfield.css')
+
+
+    .combine([
+        'client/resources/js/leaflet.js',
+        'client/resources/js/leaflet.markercluster.js',
+        'client/resources/js/leaflet-search.js',
+        'client/resources/js/proj4.js',
+        'client/resources/js/GridFieldMap.js',
+    ], 'client/dist/js/gridfieldmap.js')
+
+    .combine([
+        'client/resources/css/leaflet.css',
+        'client/resources/css/MarkerCluster.css',
+        'client/resources/css/MarkerCluster.Default.css',
+        'client/resources/css/leaflet-search.css',
+    ], 'client/dist/js/gridfieldmap.css')
+
+;
